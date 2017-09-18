c9b2875b54fc9d089fece998be97a90600570e27
Rob Underwood <runderwood5@gmail.com>
2017-09-17 21:30:53 -0400

    Fixed Star Wars (A New Hope) image

--- api/MovieImages.js
+++ api/MovieImages.js
@@ -1,5 +1,5 @@
 const movie_image_uris = {
-	"A New Hope": "https://en.wikipedia.org/wiki/File:StarWarsMoviePoster1977.jpg",
+	"A New Hope": "https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",
 	"The Empire Strikes Back": "http://cdn3.volusion.com/bxqxk.xvupj/v/vspfiles/photos/SIXSHEET175-2.jpg",
 	"defaultpic": "http://img.lum.dolimg.com/v1/images/starwars_551c43f4.jpeg"
 }
