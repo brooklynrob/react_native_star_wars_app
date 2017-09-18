97e203fdddb614b5ee4d85f0ee3ffaa7ed579ad4
Rob Underwood <runderwood5@gmail.com>
2017-09-17 21:25:06 -0400

    Added Empire Strikes Back Image URI

--- api/MovieImages.js
+++ api/MovieImages.js
@@ -1,10 +1,15 @@
 const movie_image_uris = {
 	"A New Hope": "https://en.wikipedia.org/wiki/File:StarWarsMoviePoster1977.jpg",
+	"The Empire Strikes Back": "http://cdn3.volusion.com/bxqxk.xvupj/v/vspfiles/photos/SIXSHEET175-2.jpg",
 	"defaultpic": "http://img.lum.dolimg.com/v1/images/starwars_551c43f4.jpeg"
 }
 
-function fetchMovieImageURI(name){
-	if (movie_image_uris[name]) { return movie_image_uris[name]; } else { return movie_image_uris["defaultpic"]; }
+function fetchMovieImageURI(title) {
+	if (movie_image_uris[title]) {
+		return movie_image_uris[title]
+	} else {
+		return movie_image_uris["defaultpic"]
+	}
 }
 
 export { fetchMovieImageURI }
